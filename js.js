8f1fc249a94fe0a19da55751c1dd67f47f6c2b07
HamidReza Mahmoudzadeh <h.nypd@yahoo.com>
2022-06-14 13:25:40 +0430

    js practice

---
+++ js.js
@@ -0,0 +1,19 @@
+// Practice 1
+
+    //Variable Set
+let x,y,z;// Statement 1
+x=4;// Statement 2
+y=5;// Statement 3
+z=x+y+95*2/25;// Statement 4
+    //Function Set
+function test(){
+    document.getElementById("p1").innerHTML="Value is "+z+" Final"
+    document.getElementById("p2").innerHTML="Value is "+(x+z)/y+" Final"
+    document.getElementById("p3").innerHTML= x +"  Fucking  "+ y
+}
+
+
+
+// Practice 2
+
+    //
